ab21cbd9fe73aab9cc0d78b20d381444f21fedb5
pbamr <89595466+pbamr@users.noreply.github.com>
2021-10-29 20:33:06 +0200

    Add files via upload

--- set.shutdown.pas
+++ set.shutdown.pas
@@ -29,6 +29,26 @@ Uses
 
 {$define VERSION_0}
 
+const
+	SYSCALL_NR		= 499;		//free syscall
+	SYSCALL_INIT		= 0;
+	SYSCALL_NEW_TIME	= 1;
+
+	MAX_YEAR 		= (9223372036854775807 div 60 div 60 div 24 div 365) - 1;
+	MAX_DAY 		= (9223372036854775807 div 60 div 60 div 24) - 1;
+	MAX_HOUR 		= (9223372036854775807 div 60 div 60) - 1;
+	MAX_MIN 		= (9223372036854775807 div 60) - 2;
+	MAX_SEC 		= 9223372036854775807 - 120;
+
+var
+	YEAR		: int64 = 0;
+	DAY		: int64 = 0;
+	HOUR		: int64 = 0;
+	MIN		: int64 = 0;
+	SHUTDOWN_TIME	: int64 = 0;
+
+
+
 
 Procedure Error_Message;
 begin
@@ -41,6 +61,13 @@ begin
 {$endif VERSION_0}
 
 	writeln;
+	writeln('MAX YEAR    : ', MAX_YEAR);
+	writeln('MAX DAY     : ', MAX_DAY);
+	writeln('MAX HOUR    : ', MAX_HOUR);
+	writeln('MAX MIN     : ', MAX_MIN);
+	writeln('MAX SEC     : ', MAX_SEC);
+	writeln;
+	writeln;
 	writeln('SET TIME    : <-S> <Sec.>');
 	writeln('SET TIME    : <-M> <Min.>');
 	writeln('SET TIME    : <-H> <Hour> <Min>');
@@ -52,31 +79,16 @@ begin
 	writeln('NEW TIME    : <-nH> <Hour> <Min>');
 	writeln('NEW TIME    : <-nD> <Day>  <Hour> <Min>');
 	writeln('NEW TIME    : <-nY> <YEAR> <Day>  <Hour> <Min>');
-	writeln(-1);
+	writeln(-9);
 	halt(1);
 end;
 
 
 
 
-var
-	YEAR		: int64 = 0;
-	DAY		: int64 = 0;
-	HOUR		: int64 = 0;
-	MIN		: int64 = 0;
-	SHUTDOWN_TIME	: int64 = 0;
-
-
-
-const
-	SYSCALL_NR		= 499;		//free syscall
-	SYSCALL_INIT		= 0;
-	SYSCALL_NEW_TIME	= 1;
-
-
-
 //simple
 begin
+
 	If ParamCount < 1 then Error_Message;
 	
 	if ParamStr(1) = '-S' then begin
@@ -84,13 +96,14 @@ begin
 
 		if TryStrToInt64(ParamStr(2), SHUTDOWN_TIME) = False then Error_Message;
 		if SHUTDOWN_TIME < 0 then Error_Message;
+		if SHUTDOWN_TIME > MAX_SEC then Error_Message;
 
 		writeln('SHUTDOWN TIME IN SEC. ', SHUTDOWN_TIME);
 
 {$ifdef VERSION_0}
 		writeln(do_SysCall(SYSCALL_NR, SYSCALL_INIT, SHUTDOWN_TIME));
 {$else VERSION_0}
-//Byte Order save? Not check.
+//Byte Order save
 		writeln(do_SysCall(SYSCALL_NR, SYSCALL_INIT, lo(SHUTDOWN_TIME), hi(SHUTDOWN_TIME)));
 {$endif VERSION_0}
 
@@ -101,9 +114,11 @@ begin
 		if ParamCount <> 2 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), SHUTDOWN_TIME) = False then Error_Message;
+		if SHUTDOWN_TIME > MAX_MIN then Error_Message;
 
 		SHUTDOWN_TIME *= 60;
 		if SHUTDOWN_TIME < 0 then Error_Message;
+		
 
 		writeln('SHUTDOWN TIME IN SEC. ', SHUTDOWN_TIME);
 
@@ -120,7 +135,10 @@ begin
 		if ParamCount <> 3 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), HOUR) = False then Error_Message;
+		if HOUR > MAX_HOUR then Error_Message;
+
 		if TryStrToInt64(ParamStr(3), MIN) = False then Error_Message;
+		if MIN > 59 then Error_Message;
 
 		SHUTDOWN_TIME := HOUR * 3600;
 		SHUTDOWN_TIME += MIN * 60;
@@ -139,8 +157,13 @@ begin
 		if ParamCount <> 4 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), DAY) = False then Error_Message;
+		if DAY > MAX_DAY then Error_Message;
+
 		if TryStrToInt64(ParamStr(3), HOUR) = False then Error_Message;
+		if DAY > 364 then Error_Message;
+
 		if TryStrToInt64(ParamStr(4), MIN) = False then Error_Message;
+		if MIN > 59 then Error_Message;
 
 		SHUTDOWN_TIME := DAY * 24 * 3600;
 		SHUTDOWN_TIME += HOUR * 3600;
@@ -160,17 +183,25 @@ begin
 		if ParamCount <> 5 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), YEAR) = False then Error_Message;
+		if YEAR > MAX_YEAR then Error_Message;
+
 		if TryStrToInt64(ParamStr(3), DAY) = False then Error_Message;
+		if DAY > 364 then Error_Message;
+
 		if TryStrToInt64(ParamStr(4), HOUR) = False then Error_Message;
+		if HOUR > 23 then Error_Message;
+
 		if TryStrToInt64(ParamStr(5), MIN) = False then Error_Message;
+		if MIN > 59 then Error_Message;
 
 		SHUTDOWN_TIME := YEAR * 365 * 24 * 3600;
 		SHUTDOWN_TIME += DAY * 24 * 3600;
 		SHUTDOWN_TIME += HOUR * 3600;
 		SHUTDOWN_TIME += MIN * 60;
-		if SHUTDOWN_TIME < 0 then Error_Message;
 
+		if SHUTDOWN_TIME < 0 then Error_Message;
 		writeln('SHUTDOWN TIME IN SEC. ', SHUTDOWN_TIME);
+
 
 {$ifdef VERSION_0}
 		writeln(do_SysCall(SYSCALL_NR, SYSCALL_INIT, SHUTDOWN_TIME));
@@ -188,6 +219,8 @@ begin
 
 		if TryStrToInt64(ParamStr(2), SHUTDOWN_TIME) = False then Error_Message;
 		if SHUTDOWN_TIME < 0 then Error_Message;
+		if SHUTDOWN_TIME > MAX_SEC then Error_Message;
+
 
 		writeln('SHUTDOWN TIME IN SEC. ', SHUTDOWN_TIME);
 {$ifdef VERSION_0}
@@ -202,6 +235,7 @@ begin
 		if ParamCount <> 2 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), SHUTDOWN_TIME) = False then Error_Message;
+		if SHUTDOWN_TIME < MAX_MIN then Error_Message;
 
 		SHUTDOWN_TIME *= 60;
 		if SHUTDOWN_TIME < 0 then Error_Message;
@@ -220,7 +254,10 @@ begin
 		if ParamCount <> 3 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), HOUR) = False then Error_Message;
+		if HOUR > MAX_HOUR then Error_Message;
+
 		if TryStrToInt64(ParamStr(3), MIN) = False then Error_Message;
+		if MIN > 59 then Error_Message;
 
 		SHUTDOWN_TIME := HOUR * 3600;
 		SHUTDOWN_TIME += MIN * 60;
@@ -239,8 +276,13 @@ begin
 		if ParamCount <> 4 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), DAY) = False then Error_Message;
+		if DAY > MAX_DAY then Error_Message;
+
 		if TryStrToInt64(ParamStr(3), HOUR) = False then Error_Message;
+		if HOUR > 23 then Error_Message;
+
 		if TryStrToInt64(ParamStr(4), MIN) = False then Error_Message;
+		if MIN > 59 then Error_Message;
 
 		SHUTDOWN_TIME := DAY * 24 * 3600;
 		SHUTDOWN_TIME += HOUR * 3600;
@@ -260,17 +302,26 @@ begin
 		if ParamCount <> 5 then Error_Message;
 
 		if TryStrToInt64(ParamStr(2), YEAR) = False then Error_Message;
+		if YEAR > MAX_YEAR then Error_Message;
+
 		if TryStrToInt64(ParamStr(3), DAY) = False then Error_Message;
+		if DAY > 364 then Error_Message;
+
 		if TryStrToInt64(ParamStr(4), HOUR) = False then Error_Message;
+		if HOUR > 23 then Error_Message;
+
 		if TryStrToInt64(ParamStr(5), MIN) = False then Error_Message;
+		if MIN > 59 then Error_Message;
 
 		SHUTDOWN_TIME := YEAR * 365 * 24 * 3600;
 		SHUTDOWN_TIME += DAY * 24 * 3600;
 		SHUTDOWN_TIME += HOUR * 3600;
 		SHUTDOWN_TIME += MIN * 60;
-		if SHUTDOWN_TIME < 0 then Error_Message;
 
+		if SHUTDOWN_TIME < 0 then Error_Message;
 		writeln('SHUTDOWN TIME IN SEC. ', SHUTDOWN_TIME);
+
+
 {$ifdef VERSION_0}
 		writeln(do_SysCall(SYSCALL_NR, SYSCALL_NEW_TIME, SHUTDOWN_TIME));
 {$else VERSION_0}
